2e648a5aec90783fa7a5a543762215c94d508463
orwel009 <orwelvincent1@gmail.com>
2024-06-12 22:41:30 +0530

    AddReview Completed

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const mongoose = require("mongoose")
 const express = require("express")
 const cors = require("cors")
 
+const reviews = require("./models/review")
+const {reviewModel} = require("./models/review")
+
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://orwel000:orwel123@cluster0.hyugd.mongodb.net/reviewDB?retryWrites=true&w=majority&appName=Cluster0")
+
 
 app.post("/add",(req,res)=>{
     let input = req.body
-    console.log(input)
-    res.send("Adde page")
+    let review = new reviewModel(input)
+    // console.log(review)
+    review.save()
+    res.json({"status":"success"})
 })
 
 app.listen(8080,()=>{
